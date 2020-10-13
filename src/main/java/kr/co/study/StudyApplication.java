9dada41ed4458ced6e713da6abafc009da821e0e
JeonYeongCh <ycjeon92@gmail.com>
2020-10-13 17:09:43 +0900

    커밋 테스트

--- src/main/java/kr/co/study/StudyApplication.java
+++ src/main/java/kr/co/study/StudyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyApplication {
 
 	public static void main(String[] args) {
+		System.out.println("시스템 스터디");
 		SpringApplication.run(StudyApplication.class, args);
 	}
 
